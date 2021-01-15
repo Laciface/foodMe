6916b086cc7c7e298693bc5021742ea446e37ed0
csongam7 <csongam7@gmail.com>
2021-01-15 09:00:40 +0100

    pictures added to home page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import HomePage from './components/HomePage'
 import FoodList from './components/FoodList'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import FoodDetails from './components/FoodDetails';
+import Laci from './pictures/laci.png';
+import csongi from './pictures/csongi.png';
 
 
 function App(){
@@ -19,13 +21,18 @@ function App(){
           <div style={{paddingBottom: '3.5rem'}}>
           <Header/>
           <Navbar/>
-          <Route exact path='/' component={HomePage} />
+          <div style={{display: "inline-block", position: "relative"}}>
+            <div style={{display: "inline-block", position: "relative"}}><Route exact path='/' component={HomePage} /></div>
+            
+          </div>
           <Route exact path='/Categories' component={Categories} />
           <Route exact path={'/Categories/:category'}><FoodList/></Route>
           <Route exact path={'/FoodDetails/:id'}><FoodDetails/></Route>
           <Footer/>
           </div>
+          
         </div>
+        
         </Router>
       </FoodProvider>
     );
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import styled from 'styled-components'
 import Search from './Search'
+import Laci from '/home/csongam7/Documents/PHP/foodMe/src/pictures/laci.png';
+import csongi from '/home/csongam7/Documents/PHP/foodMe/src/pictures/csongi.png';
 
 export default function HomePage() {
     return (
         <React.Fragment>
             <div>
-                <Welcome>Welcome!</Welcome>
+            <div style={{display: "inline-block", position: "relative"}}>
+            <div style={{display: "inline-block", position: "relative", float: "right"}}><img src={Laci} width="200" height="300"/></div>
+            <Welcome style={{display: "inline-block", position: "relative"}}>Welcome!</Welcome>
+            <div style={{display: "inline-block", position: "relative", float: "left"}}><img src={csongi} width="250" height="350"/></div>
+            
+          </div>
+                
             </div>
             <div>
                 <Search/>
