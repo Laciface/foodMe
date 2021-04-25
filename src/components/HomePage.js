7aad19d0fe479329d38e5c03d0074193cdb0ea40
Laciface <csomali18@gmail.com>
2021-04-25 21:37:55 +0200

    refactor: delete dead code

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,18 +1,8 @@
 import React from 'react'
-import axios from 'axios'
-import { useState, useEffect, useContext} from 'react'
 import SlideShow from '../components/SlideShow'
 
 
 export default function HomePage() {
-
-    const [name, setName] =  useState();
-
-    const welcome =() => {
-        axios.get('http://127.0.0.1:8000/api/name')
-        .then(resp => setName(resp.data))
-};
-
     return (
         <React.Fragment>
             <SlideShow/>
