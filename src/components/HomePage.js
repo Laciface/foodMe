31c3a30220e20c128a7a12851f39dbb492690f32
Laciface <csomali18@gmail.com>
2021-04-27 10:10:45 +0200

    add: title to the app

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import SlideShow from '../components/SlideShow'
-
+import { Helmet } from 'react-helmet';
 
 export default function HomePage() {
     return (
         <React.Fragment>
+        <Helmet>
+            <title>FoodMe</title>
+        </Helmet>
             <SlideShow/>
         </React.Fragment>
     )
