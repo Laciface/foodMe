3c9dae0648948d4f82016a3ad07f1be9b0c9ab2c
Laciface <csomali18@gmail.com>
2021-05-19 18:21:17 +0200

    feat: addRecipe form is ready

--- src/components/Profile/AddRecipe.js
+++ src/components/Profile/AddRecipe.js
@@ -3,34 +3,52 @@ import TextField from '@material-ui/core/TextField';
 import styled from 'styled-components';
 
 export default function AddRecipe() {
+    const [ingredient, setIngredient] = useState('');
+    const [measure, setMeasure] = useState('');
     const [ingredList, setIngredList] = useState([]);
-    const [numberOfFields, setNumberOfFields] = useState(1);
+    let [counter, setCounter] = useState(1);
 
-    const setIngredient = (event) => {
-        let list = ingredList;
-        list.push(event.target.value);
-		setIngredList(list);
+
+    const saveIngredient = (event) => {
+		setIngredient(event.target.value);
 	};
 
-    const displayFields = () =>{
-        let content = [];
-        for(let i = 0; i < numberOfFields; i++){
-            content.push(
-                <div style={{display: 'flex', flexDirection: 'row', marginBottom: '15px'}}>
-                <TextField onChange={setIngredient} id="outlined-search" type="normal" variant="outlined" style={{marginRight: '25px'}}/>
-                <TextField id="outlined-search" type="normal" variant="outlined" />
-            </div>
-            );}
-        return (
-            <React.Fragment>
-            {content.map(item => item)}
-            </React.Fragment>
-        );
+    const saveMeasure = (event) => {
+		setMeasure(event.target.value);
+	};
+
+    const saveFields = (event) => {
+        event.preventDefault();
+        ingredList[`strIngredient${counter}`] = ingredient;
+        ingredList[`strMeasure${counter}`] = measure;
+        setCounter(counter + 1);
+        event.target.value = '';
     }
 
-    const increseNumberOfFields = (e) => {
-        e.preventDefault();
-        setNumberOfFields(numberOfFields + 1);
+    const  displayMeasures = () => {
+        const list =[];
+        Object.keys(ingredList).forEach(key => {
+            if(key.includes("strMeasure")){
+                list.push(ingredList[key]);
+            }
+            });
+
+        const listItems = list.map((item) => 
+        <LI>{item}</LI>)
+        return listItems;
+    }
+
+    const displayIngredients = () => {
+        const list =[];
+        Object.keys(ingredList).forEach(key => {
+            if(key.includes("strIngredient")){
+                list.push(ingredList[key]);
+            }
+            });
+
+        const listItems = list.map((item) => 
+        <LI>{item}</LI>)
+        return listItems;
     }
 
     return (
@@ -40,19 +58,33 @@ export default function AddRecipe() {
                         <Container>
                             <Label>Name</Label>
                             <TextField id="outlined-search" type="normal" variant="outlined" />
-                            {console.log(ingredList)}
                             <Label>Origin</Label>
                             <TextField id="outlined-search" type="normal" variant="outlined" />
                             
                             <Label style={{marginBottom: '10px'}}>Category</Label>
                             <select>
-                                <option>1</option>
-                                <option>2</option>
-                                <option>3</option>
+                                <option value="Beef">Beef</option>
+                                <option value="Breakfast">Breakfast</option>
+                                <option value="Chicken">Chicken</option>
+                                <option value="Dessert">Dessert</option>
+                                <option value="Goat">Goat</option>
+                                <option value="Lamb">Lamb</option>
+                                <option value="Miscellaneous">Miscellaneous</option>
+                                <option value="Pasta">Pasta</option>
+                                <option value="Pork">Pork</option>
+                                <option value="Seafood">Seafood</option>
+                                <option value="Side">Side</option>
+                                <option value="Starter">Starter</option>
+                                <option value="Vegan">Vegan</option>
+                                <option value="Vegaterian">Vegaterian</option>
                             </select>
                             
                             <Label>Instructions</Label>
-                            <textarea style={{height: '300px', width: '500px'}}  ></textarea>
+                            <textarea style={{height: '200px', width: '500px'}}  ></textarea>
+                            
+                            <Label>Youtube Link</Label>
+                            <TextField id="outlined-search" type="normal" variant="outlined" />
+                            
                             <Button type="submit" >Add Recipe </Button>
                         </Container>
                         <div>
@@ -61,11 +93,16 @@ export default function AddRecipe() {
                                 <input type="file"/>
 
                                 <Label>Ingredients/Measures</Label>
-                                {displayFields()}
+                                <div style={{display: 'flex', flexDirection: 'row', marginBottom: '15px'}}>
+                                    <TextField onChange={saveIngredient} id="outlined-search" type="normal" variant="outlined" style={{marginRight: '25px'}}/>
+                                    <TextField onChange={saveMeasure} id="outlined-search" type="normal" variant="outlined" />
+                                    <Button onClick={saveFields}>+</Button>
+                                </div>
+                                <Items>
+                                    <OL>{displayIngredients()}</OL>
+                                    <UL>{displayMeasures()}</UL>
+                                </Items>
                             </Container>
-                        </div>
-                        <div>
-                        <Button onClick={increseNumberOfFields}>+</Button>
                         </div>
                     </div>
                 </form>
@@ -73,8 +110,32 @@ export default function AddRecipe() {
     )
 }
 
+const LI = styled.li`
+    margin-bottom: 15px;
+`
+
+const UL = styled.ul`
+    list-style-type: none;
+    margin-top: 70px;
+`
+
+const OL = styled.ol`
+    margin-right: 0;
+    margin-top: 70px;
+    margin-left: 70px;
+`
+
+const Items = styled.div`
+    height: 470px;
+    width: 500px;
+    background-repeat: no-repeat;
+    background-size: 500px;
+    display: flex;
+    flex-direction: row;
+    background-image: url('https://lh3.googleusercontent.com/proxy/KirkilQ6b0kpZeOWcfVQ6l74-7pP8j4I_6IpxTRoxs4lUH320PHZc5eih5jR3cDIKadDg6MB5SIjQkoWwKNgQ3k')
+`
+
 const Button = styled.button`
-    width: 200px;
     text-decoration: none;
     font-family: 'sans-serif';
     font-size: 1.3rem;
@@ -94,6 +155,7 @@ const Label = styled.label`
     text-align: left;
     font-family:'Gloria Hallelujah', cursive;
     font-size: 25px;
+    margin-bottom: 10px;
 `
 
 const Container = styled.div`
